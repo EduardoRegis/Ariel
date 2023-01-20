119578b5bedb9598df064d9d69af942e0f0fc6a0
Eduardo Regis <eduardo.regis@mblabs.com.br>
2023-01-20 11:37:29 -0300

    make modules for menu and splash screens

---
+++ Ariel/Modules/Splash/SplashScreenViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SplashScreenViewController.swift
+//  Ariel
+//
+//  Created by Eduardo Oliveira on 20/01/23.
+//
+
+import UIKit
+
+class SplashScreenViewController: BaseViewController {
+
+    // MARK: - Outlets
+    
+    
+    // MARK: - Properties
+    var presenter: SplashScreenPresenter!
+    
+    // MARK: - View Lifecycle
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.didLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter.willAppear()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        presenter.didAppear()
+    }
+    
+    // MARK: - Methods
+
+    // MARK: - Actions
+    func navigateToCardList() {
+        self.presenter.navigateToMenu()
+    }
+}
+
+// MARK: - SplashScreenPresenterDelegate
+extension SplashScreenViewController: SplashScreenPresenterDelegate {
+    func didLoadRemoteConfig() {
+    }
+}
