a23b1c4cfbdf1efeda6c5ffbf4833fe9e8b7cc72
Eduardo Regis <eduardo.regis@mblabs.com.br>
2023-01-24 22:00:15 -0300

    finish credit screen

--- Ariel/Modules/Credits/CreditsScreenPresenter.swift
+++ Ariel/Modules/Credits/CreditsScreenPresenter.swift
@@ -29,6 +29,25 @@ class CreditsScreenPresenter {
     func didAppear() {
     }
     
+    func memberFunction(index: Int) -> String {
+        switch index {
+        case 0:
+            return "Avaliador, Roteirista, Ilustrador"
+        case 1:
+            return "Revisor, Ilustrador, Designer de Som"
+        case 2:
+            return "Programador"
+        case 3:
+            return "Líder, Roteirista, Ilustradora"
+        case 4:
+            return "Designer de Personagem, Designer de\n Interface, Ilustrador"
+        case 5:
+            return "Designer de Interface, Designer UX"
+        default:
+            return ""
+        }
+    }
+    
     func backToMenu() {
         self.router.backToMenu()
     }
